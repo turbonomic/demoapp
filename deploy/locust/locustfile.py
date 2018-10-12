ea0f038b86cad57f1d1439d89d0b99bc56151f63
max_wang <max.wang@turbonomic.com>
2018-10-12 13:53:24 -0400

    Support weekly, daily, hourly, and minutely patterns for varied workload.

--- deploy/locust/locustfile.py
+++ deploy/locust/locustfile.py
@@ -1,28 +1,55 @@
 import os
 from locust import HttpLocust, TaskSet, task
 import random
-from datetime import datetime
+import time
 import logging
 
 MAX_TWITTER_USER_ID = int(os.getenv('MAX_TWITTER_USER_ID', 101))
 MAX_NUM_FOLLOWS = int(os.getenv('MAX_NUM_FOLLOWS', 50))
-VISIT_RATE_TWEET = int(os.getenv('VISIT_RATE_TWEET', 1000))
-VISIT_RATE_TIMELINE = int(os.getenv('VISIT_RATE_TIMELINE', 100))
-VISIT_RATE_NEWSFEED = int(os.getenv('VISIT_RATE_NEWSFEED', 1000))
-VISIT_RATE_FOLLOW = int(os.getenv('VISIT_RATE_FOLLOW', 10))
+VISIT_RATE_TWEET = int(os.getenv('VISIT_RATE_TWEET', 0))
+VISIT_RATE_TIMELINE = int(os.getenv('VISIT_RATE_TIMELINE', 0))
+VISIT_RATE_NEWSFEED = int(os.getenv('VISIT_RATE_NEWSFEED', 100))
+VISIT_RATE_FOLLOW = int(os.getenv('VISIT_RATE_FOLLOW', 0))
 NEXT_REQUEST_MIN_WAIT_MS = int(os.getenv('NEXT_REQUEST_MIN_WAIT_MS', 1000))
-NEXT_REQUEST_MAX_WAIT_MS = int(os.getenv('NEXT_REQUEST_MAX_WAIT_MS', 10000))
+NEXT_REQUEST_MAX_WAIT_MS = int(os.getenv('NEXT_REQUEST_MAX_WAIT_MS', 3000))
 
+logging.info("\n\tMAX_TWITTER_USER_ID=%s\n\tMAX_NUM_FOLLOWS=%s\n\tVISIT_RATE_TWEET=%s\n\tVISIT_RATE_TIMELINE=%s"
+             "\n\tVISIT_RATE_NEWSFEED=%s\n\tVISIT_RATE_FOLLOW=%s\n\tNEXT_REQUEST_MIN_WAIT_MS=%s\n\tNEXT_REQUEST_MAX_WAIT_MS=%s",
+             MAX_TWITTER_USER_ID, MAX_NUM_FOLLOWS, VISIT_RATE_TWEET, VISIT_RATE_TIMELINE,
+             VISIT_RATE_NEWSFEED, VISIT_RATE_FOLLOW, NEXT_REQUEST_MIN_WAIT_MS, NEXT_REQUEST_MAX_WAIT_MS)
+
+# TIME_SLOT_PROBABILITY is used to simulate varied workload, where for each time slot, the probability that a user
+# will fire a request is the probability specified in the time slot.
+# Examples:
+#   hourly pattern with 10-minute step: TIME_SLOT_PROBABILITY = '1.0,0.7,0.4,0.1,0.4,0.7'
+#                                       TIME_SLOT_PROBABILITY_STEP_IN_MIN = '10'
+#
+#   6-hour pattern with one-hour step:  TIME_SLOT_PROBABILITY = '1.0,0.7,0.4,0.1,0.4,0.7'
+#                                       TIME_SLOT_PROBABILITY_STEP_IN_MIN = '60'
+#
+#   daily pattern with 12-hour step:    TIME_SLOT_PROBABILITY = '1.0,0'
+#                                       TIME_SLOT_PROBABILITY_STEP_IN_MIN = '720'
+#
+#   weekly pattern with one-day step:   TIME_SLOT_PROBABILITY = '1.0,0.9,0.9,0.9,0.8,0.3,0.2'
+#                                       TIME_SLOT_PROBABILITY_STEP_IN_MIN = '1440'
 ENABLE_TIME_SLOT_PROBABILITY = os.getenv('ENABLE_TIME_SLOT_PROBABILITY', '0') != '0'
-TIME_SLOT_PROBABILITY = list(map(lambda p:int(10*float(p)), os.getenv('TIME_SLOT_PROBABILITY', '1.0,0.7,0.4,0.1,0.4,0.7').split(',')))
+TIME_SLOT_PROBABILITY = list(map(lambda p:int(100*float(p)), os.getenv('TIME_SLOT_PROBABILITY', '1.0,0.7,0.4,0.1,0.4,0.7').split(',')))
+TIME_SLOT_PROBABILITY_STEP_IN_MIN = int(os.getenv('TIME_SLOT_PROBABILITY_STEP_IN_MIN', '60'))
+TIME_SLOT_CYCLE_IN_MIN = len(TIME_SLOT_PROBABILITY) * TIME_SLOT_PROBABILITY_STEP_IN_MIN
+
+if ENABLE_TIME_SLOT_PROBABILITY:
+    logging.info("Varied workload with %s-minute cycle is enabled: prob = %s with step = %s minutes",
+                 TIME_SLOT_CYCLE_IN_MIN, TIME_SLOT_PROBABILITY, TIME_SLOT_PROBABILITY_STEP_IN_MIN)
 
 def time_to_fire():
     if not ENABLE_TIME_SLOT_PROBABILITY:
         return True
 
-    m = datetime.now().minute // 10
+    t = time.localtime()
+    m = ((t.tm_wday * 24 + t.tm_hour) * 60 + t.tm_min) % TIME_SLOT_CYCLE_IN_MIN // TIME_SLOT_PROBABILITY_STEP_IN_MIN
+
     p = TIME_SLOT_PROBABILITY[m]
-    return p >= random.randint(1,10)
+    return p >= random.randint(1,100)
 
 filename = os.getenv('TWEET_CONTENT_FILE', '/comments.txt')
 comments = []
@@ -43,9 +70,6 @@ class UserBehavior(TaskSet):
         self.timeline_endpoint = "/timeline"
         self.newsfeed_endpoint = "/newsfeed"
         self.follows_endpoint = "/follows"
-
-        if ENABLE_TIME_SLOT_PROBABILITY:
-            logging.info("Time slot probability is enabled: %s", TIME_SLOT_PROBABILITY)
 
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
